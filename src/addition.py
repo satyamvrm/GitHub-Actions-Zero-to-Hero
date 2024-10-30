e5d7ab02feda936a6285209bd8140530a7ceb270
Satyam Verma <satyamvrm@outlook.com>
2024-10-30 15:22:25 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,17 @@
 # app.py
 # This is a test commit
+#This is a addition function
 def add(a, b):
     return a + b
+
+#this is a subtraction function
+def sub(a,b):
+    return a-b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def test_sub():
+    assert sub(2,1) == 1
+    assert sub(5,2) == 3
